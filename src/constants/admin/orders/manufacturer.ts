e03a3f57495a86a4a4ca953672a0ddcd11af0270
WDCeriga <wdceriga@outlook.com>
2025-03-18 09:33:01 +0000

    added individual manu

--- src/constants/admin/orders/manufacturer.ts
+++ src/constants/admin/orders/manufacturer.ts
@@ -4,4 +4,7 @@ export const manufacturerStore: manufacturerType[] = [
   "Portugal  manufacturer",
   "Türkiye manufacturer",
   "Spain manufacturer",
+  "Samuul O Assigned",
+  "Richy J Assigned",
+  "Sam E Assigned",
 ];
